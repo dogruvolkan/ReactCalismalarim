e4d5fd4f92ea87dc19ca72ab0fb073fb4fd7f9af
volkan <volkandogru11@gmail.com>
2022-07-01 18:43:35 +0300

    component react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello React</h1>
+      <Header></Header>
+
+      <label htmlFor="myinput">Name</label>
+      <input id="myinput"></input>
     </div>
-  );
+  )
 }
 
 export default App;
